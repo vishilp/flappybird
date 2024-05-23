dc6fefc80b311c8170e60ab4a232da1674eca13f
vishilp <vishilthe3rd@gmail.com>
2024-05-22 21:36:35 -0700

    pipe class

--- flappybird.py
+++ flappybird.py
@@ -49,7 +49,7 @@ class Bird:
                 self.tilt = self.max_rotation
         else:                                               #moving down
             if self.tilt > -90:
-                self.tilte -= self.rot_vel
+                self.tilt -= self.rot_vel
 
     def draw(self, window):
         self.img_count +=1
@@ -76,6 +76,34 @@ class Bird:
     def get_mask(self):
         return pygame.mask.from_surface(self.img)
 
+class Pipe:
+    gap= 200
+    vel= -5
+
+    def __init__(self, x):
+        self.x= x
+        self.height= 0
+        self.top=0
+        self.bottom= 0
+        self.pipe_top = pygame.transform.flip(PIPE_IMG, False, True)
+        self.pipe_bottom= PIPE_IMG
+        self.passed= False
+        self.set_height()
+
+    def set_height(self):
+        self.height= random.randrange(40,450)
+        self.top= self.height - self.pipe_top.get_height()
+        self.bottom = self.height + self.gap
+    
+    def move(self):
+        self.x += self.vel
+
+    def draw(self,window):
+        window.blit(self.pipe_top, (self.x, self.top))
+        window.blit(self.pipe_bottom, (self.x, self.bottom))
+
+
+
 def draw_window(window, bird):
     window.blit(BG_IMG, (0,0))
     bird.draw(window)
@@ -85,11 +113,14 @@ def main():
     flappybird = Bird(200,200)
     run = True
     window = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
+    clock = pygame.time.Clock()
 
     while run:
+        clock.tick(30)  #at most 30 ticks in one second
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False   
+        flappybird.move()
         draw_window(window, flappybird)
 
     pygame.quit()
